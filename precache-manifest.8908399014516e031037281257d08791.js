6125a79dd0c9ef890536bf81bec013736282d2ca
tykto <tykto.login@gmail.com>
2018-11-16 18:05:16 +1100

    updated via CI

---
+++ precache-manifest.8908399014516e031037281257d08791.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0da40afe804eca430684",
+    "url": "/static/js/main.0da40afe.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "0da40afe804eca430684",
+    "url": "/static/css/main.4914dbd5.chunk.css"
+  },
+  {
+    "revision": "7a842148dcb3495fb3d9f438a1a54b3c",
+    "url": "/index.html"
+  }
+];
