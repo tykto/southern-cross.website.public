9df7b8dca89b526c71a1ff4936466007b289362e
tykto <tykto.login@gmail.com>
2018-11-16 20:09:57 +1100

    updated via CI

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/southern-cross.website.public/precache-manifest.c91c23dc08202d83aa37711bf4c8fa2e.js"
+  "/southern-cross.website.public/precache-manifest.8ca9044695a58647204f0d87f7654283.js"
 );
 
 workbox.clientsClaim();
