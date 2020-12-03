b109a8866a7dc94535febbb589ee4f869f37208e
Andrew Robertson <andrewcrobertson@gmail.com>
2020-12-04 10:46:24 +1100

    refactor: add layout

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,12 +2,4 @@
   import HomePage from '@app/pages/Home';
 </script>
 
-<svelte:head>
-  <meta name="description" content="Rubbish removal" />
-  <meta name="author" content="Southern Cross Rubbish Removal" />
-  <meta name="keywords" content="Rubbish removal" />
-  <meta name="robots" content="index, follow" />
-  <title>Southern Cross :: Rubbish Removal</title>
-</svelte:head>
-
 <HomePage />
