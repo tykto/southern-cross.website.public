4abec6a7a25cb8ee96323cf52859a240208fb35d
Lara Robertson <laraejrobertson@gmail.com>
2020-12-03 12:00:47 +1100

    refactor: hero styles

--- src/routes/_components/hero.svelte
+++ src/routes/_components/hero.svelte
@@ -2,6 +2,43 @@
 </script>
 
 <style>
+  .hero {
+    min-height: 600px;
+    background: url('/images/hero.jpg') no-repeat center center;
+    background-size: cover;
+    margin-top: -186px;
+  }
+
+  .tagline {
+    padding-right: 20px;
+    padding-bottom: 20px;
+    color: #ffa401;
+    font-family: 'Caveat', cursive;
+    font-size: 2rem;
+  }
+
+  @media (min-width: 768px) {
+    .tagline {
+      font-size: 3rem;
+      padding-right: 50px;
+    }
+  }
+
+  @media (min-width: 992px) {
+    .tagline {
+      font-size: 4rem;
+      padding-right: 100px;
+      padding-bottom: 50px;
+    }
+  }
+
+  @media (min-width: 1200px) {
+    .tagline {
+      font-size: 5rem;
+      padding-right: 100px;
+      padding-bottom: 80px;
+    }
+  }
 </style>
 
 <div class="hero d-flex justify-content-end align-items-end">
